6a66facd298078fc9293b47f438c0c18aa2108f3
Ted <heinhtetnaing186@gmail.com>
2022-11-14 21:08:16 +0630

    c

--- config/server.js
+++ config/server.js
@@ -1,12 +1,7 @@
 module.exports = ({ env }) => ({
-
-    url: env("MY_HEROKU_URL"),
-
-    proxy: true,
-
-    app: {
-
-      keys: env.array("APP_KEYS",['keys','keys']
-},
-
-  });
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1336),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
+});
